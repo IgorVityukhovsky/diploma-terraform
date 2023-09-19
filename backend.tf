7ef41ed4e4d08f963b8abaea5f4a971045cee394
IgorVityukhovsky <104152730+IgorVityukhovsky@users.noreply.github.com>
2023-09-19 18:21:33 +0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,7 +5,7 @@ terraform {
       region   = "ru-central1-a"
       key      = "testfolder/terraform.tfstate"
       access_key = "YCAJEetwy6-Go_N3HGQAk0gTZ"
-      secret_key = "тут мой секретный ключ"
+      secret_key = "${var.TF_VAR_KEY_S3}"
       
 
       skip_region_validation      = true
